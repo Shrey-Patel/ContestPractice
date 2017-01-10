45c59d507fc6850c4815f5ae0de8e580ec039b60
Michael Bridges <msbridges99@gmail.com>
2017-01-09 18:03:53 -0800

    placed the sieve in a separate function

--- Code/FactorialFactors.cpp
+++ Code/FactorialFactors.cpp
@@ -15,6 +15,8 @@ for each prime number p
 #include <cmath>
 using namespace std;
 
+int perform_sieve(int data);
+
 int main(){
     int n;
     int result = 0;
@@ -22,31 +24,37 @@ int main(){
     //TODO: Change to read from a file
     cout << "Enter number: ";
     cin >> n;
-    bool primes[n+1];
-
-    for(int i = 0; i < n+1; i++){
-        primes[i] = true;
-    }
-
-    primes[0] = false;
-    primes[1] = false;
-
-    int p = 2;
-
-    while(p <= n){
-        for(int i = p+p; i < n + 1; i += p){
-            primes[i] = false;
-        }
-
-        int maxI = log(n)/log(p);
-        for(int i = 1; i < maxI + 1; i++){
-            result += n/(pow(p, i));
-        }
-
-        do{
-            p++;
-        } while(primes[p] == false);
-    }
-
+    result = perform_sieve(n);
     cout << "Result is: " << result << endl;
+}
+
+int perform_sieve(int n){
+	
+	int result = 0;
+	bool primes[n+1];
+
+	for(int i = 0; i < n+1; i++){
+ 		primes[i] = true;
+  	  }
+
+ 	   primes[0] = false;
+ 	   primes[1] = false;
+
+ 	   int p = 2;
+
+	    while(p <= n){
+	        for(int i = p+p; i < n + 1; i += p){
+ 	           primes[i] = false;
+  	      }
+
+   	     int maxI = log(n)/log(p);
+    	    for(int i = 1; i < maxI + 1; i++){
+     	       result += n/(pow(p, i));
+    	    }
+
+    	    do{
+      	      p++;
+      	  } while(primes[p] == false);
+   	 }
+	return result
 }
